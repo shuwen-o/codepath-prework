ba0acfc4d49457dd19c74e7d9bb56abfd1ce6960
Glitch (held-continuous-era) <none>
2022-04-11 02:48:08 +0000

    🥈🧱 Updated with Glitch

---
+++ script.js
@@ -0,0 +1,167 @@
+/* If you're feeling fancy you can add interactivity 
+    to your site with Javascript */
+
+// global constants
+const clueHoldTime = 1000; //how long to hold each clue's light/sound
+const cluePauseTime = 333; //how long to pause in between clues
+const nextClueWaitTime = 1000; //how long to wait before starting playback of the clue sequence
+
+//Global Variables
+var pattern = []
+var number = 0;
+//var pattern = [2, 2, 4, 3, 2, 1, 2, 4];
+var progress = 0; 
+var error = 0;
+var gamePlaying = false;
+var tonePlaying = false;
+var volume = 0.5;  //must be between 0.0 and 1.0
+var guessCounter = 0;
+
+//helper function to create a random pattern
+function randomizePattern(){
+  //generate 8 random numbers and add to pattern
+  for (let i=0; i<=7; i++) {
+    number = Math.floor(Math.random() * 5) + 1;
+    pattern.push(number);
+  }
+}
+
+function startGame(){
+  //create a new pattern for every game
+  pattern = [];
+  randomizePattern();
+  
+  //initialize game variables
+  progress = 0;
+  gamePlaying = true;
+  error = 0;
+  
+  // swap the Start and Stop buttons
+  document.getElementById("startBtn").classList.add("hidden");
+  document.getElementById("stopBtn").classList.remove("hidden");
+  
+  playClueSequence()
+}
+
+function stopGame(){
+  gamePlaying = false;
+  
+  document.getElementById("startBtn").classList.remove("hidden");
+  document.getElementById("stopBtn").classList.add("hidden");
+}
+
+// Sound Synthesis Functions
+const freqMap = {
+  1: 261.6, //C
+  2: 293.7, //D
+  3: 329.6, //E
+  4: 349.2, //F
+  5: 392 //G
+}
+function playTone(btn,len){ 
+  o.frequency.value = freqMap[btn]
+  g.gain.setTargetAtTime(volume,context.currentTime + 0.05,0.025)
+  context.resume()
+  tonePlaying = true
+  setTimeout(function(){
+    stopTone()
+  },len)
+}
+function startTone(btn){
+  if(!tonePlaying){
+    context.resume()
+    o.frequency.value = freqMap[btn]
+    g.gain.setTargetAtTime(volume,context.currentTime + 0.05,0.025)
+    context.resume()
+    tonePlaying = true
+  }
+}
+function stopTone(){
+  g.gain.setTargetAtTime(0,context.currentTime + 0.05,0.025)
+  tonePlaying = false
+}
+
+function lightButton(btn){
+  document.getElementById("button"+btn).classList.add("lit")
+}
+function clearButton(btn){
+  document.getElementById("button"+btn).classList.remove("lit")
+}
+
+function playSingleClue(btn){
+  if(gamePlaying){
+    lightButton(btn);
+    playTone(btn,clueHoldTime);
+    setTimeout(clearButton,clueHoldTime,btn);
+  }
+}
+
+function playClueSequence(){
+  guessCounter = 0;
+  //context.resume()
+  let delay = nextClueWaitTime; //set delay to initial wait time
+  for(let i=0;i<=progress;i++){ // for each clue that is revealed so far
+    console.log("play single clue: " + pattern[i] + " in " + delay + "ms")
+    setTimeout(playSingleClue,delay,pattern[i]) // set a timeout to play that clue
+    delay += clueHoldTime 
+    delay += cluePauseTime;
+  }
+}
+
+function loseGame(){
+  stopGame();
+  alert("Game Over. You lost.");
+}
+
+function winGame(){
+  stopGame();
+  alert("Game Over. You won!");
+}
+
+function errorMade(){
+  alert("You just made an error. You have " + (3 - error) + " chance(s) left.")
+}
+
+function guess(btn){
+  console.log("user guessed: " + btn);
+  if(!gamePlaying){
+    return;
+  }
+  
+  if(pattern[guessCounter] == btn){
+    //Guess was correct!
+    if(guessCounter == progress){
+      if(progress == pattern.length - 1){
+        winGame();
+      } else
+      {
+        progress++;
+        playClueSequence();
+      }
+    } else
+    {
+      guessCounter++;
+    }
+  } else{
+    //Guess was wrong, add to errors
+    error++;
+    //Player loses after 3 mistakes
+    if (error == 3) {
+      loseGame();
+    } else {
+      errorMade();
+      playClueSequence();
+    }
+  }
+}
+
+// Page Initialization
+// Init Sound Synthesizer
+var AudioContext = window.AudioContext || window.webkitAudioContext 
+var context = new AudioContext()
+var o = context.createOscillator()
+var g = context.createGain()
+g.connect(context.destination)
+g.gain.setValueAtTime(0,context.currentTime)
+o.connect(g)
+o.start(0)
